24188bc338a5e2f0914ce85bc799857bdc0930f8
danyewest97 <dane20052008@gmail.com>
2024-09-10 13:01:36 -0700

    quick commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	
+	// A variable that contains the HTML for the main navbar
+	var nav = '<nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top justify-content-center" width="10px">' +
+'			<div class="container-fluid">' +
+'				<div class="collapse navbar-collapse" id="collapsibleNavbar">' +
+'					<!-- Jacques\' Bagels brand logo on the navbar -->' +
+'					<a class="navbar-brand" href="index.html"> ' +
+'						<img id="nav-logo" src ="logo.png" alt="Jacques\' Bagels logo, stylized JB">' +
+'					</a>' +
+'					' +
+'					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">' +
+'					  <span class="navbar-toggler-icon"></span>' +
+'					</button>' +
+'					' +
+'					<!-- Navbar elements -->' +
+'					<ul class="navbar-nav">' +
+'					' +
+'						<!-- Items in the navbar -->' +
+'						<li class="nav-item">' +
+'							<a class="nav-link" href="index.html">Home</a>' +
+'						</li>' +
+'						<li class="nav-item">' +
+'							<a class="nav-link" href="reservations.html">Reservations</a>' +
+'						</li>' +
+'						<li class="nav-item">' +
+'							<a class="nav-link" href="menu.html">Menu</a>' +
+'						</li>' +
+'					  	' +
+'						' +
+'						<!-- Navbar dropdown menu, may get removed later -->' +
+'						<li class="nav-item dropdown">' +
+'							<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">More</a>' +
+'							<ul class="dropdown-menu">' +
+'								<li><a class="dropdown-item" href="orders.html">Online orders</a></li>' +
+'								<li><a class="dropdown-item" href="contacts.html">Contact us</a></li>' +
+'							</ul>' +
+'						</li>' +
+'					</ul>' +
+'				</div>' +
+'			' +
+'			</div>' +
+'		</nav>';
+
+	//adding this variable to the first element with the id mainBar
+	var mainBar = document.getElementById('mainBar');
+	mainBar.insertAdjacentHTML('beforeend', nav);
+	
+	// This code is much simpler with JQuery, however it makes the navbar glitch when the page is reloaded or when switching pages
+	// $("#mainBar").append(nav);
+});
