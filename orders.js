dd25b8f85b6d33fc607a11be50125e0d0f8436e2
danyewest97 <dane20052008@gmail.com>
2024-09-11 14:01:40 -0700

    Update orders.js to add more framework for processing orders properly

--- orders.js
+++ orders.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	
+	
+	
 	//An array of all available toppings (more will be/need to be added)
 	const toppings = [
 		"lemon pepper",
@@ -29,11 +31,12 @@ $(document).ready(function() {
 	
 	//This is a list of the available custom items that can be created and purchased by the user
 	//For actually ordering, change the properties of these objects to what the user inputs before adding
-	//them to the cart 
+	//them to the cart
 	//When they are added to the cart, they will be reset so that new custom items can be made by the user
 	
 	//This is a customBagel object that stores an array of toppings
 	const customBagel = {
+		name: "bagel", //the name used to refer to this object in the datasets of HTML elements
 		//common things on a bagel that can be changed by the user
 		toppings: [],
 		toasted: true,
@@ -53,6 +56,7 @@ $(document).ready(function() {
 	};
 	
 	const customDonut = {
+		name: "donut", //the name used to refer to this object in the datasets of HTML elements
 		//common things on a bagel that can be changed by the user
 		toppings: [],
 		warmed: true,
@@ -72,6 +76,7 @@ $(document).ready(function() {
 	};
 	
 	const customCroissant = {
+		name: "croissant", //the name used to refer to this object in the datasets of HTML elements
 		//common things on a bagel that can be changed by the user
 		toppings: [],
 		warmed: true,
@@ -94,6 +99,7 @@ $(document).ready(function() {
 	
 	//only 1 layer for now, probably won't add multiple layers for this project
 	const customSandwich = {
+		name: "sandwich", //the name used to refer to this object in the datasets of HTML elements
 		//common things on a bagel that can be changed by the user
 		toppings: [],
 		toasted: true,
@@ -114,6 +120,14 @@ $(document).ready(function() {
 		}
 	};
 	
+	
+	//An array of all custom items, used for handling orders
+	const availableItems = [
+		customBagel,
+		customDonut,
+		customCroissant,
+		customSandwich
+	];
 	
 	
 	//When the order HTML is done, p needs to be changed to a different selector (maybe #customBagel or #customSandwich, etc.)
@@ -171,6 +185,36 @@ $(document).ready(function() {
 	};
 	
 	
+	//Here is all the code that actually stores the user's selected items (selected items do not save on page reload)
+	
+	//When an item (not the customItems, instead things like a topping or bagel type) is clicked on, checks the
+	//data of that item and adds it to the appropriate customItem
+	$(".item").click(function() {
+		var parentType = this.dataset.parent-type; //the parent item type of the item (bagel, donut, croissant, sandwich)
+		var type = this.dataset.type; //the actual type of the item (topping, bread, etc.)
+		var value = this.dataset.value; //the actual value of the topping (i.e. lemon pepper would have a value of "lemon pepper")
+		
+		addToCustomItem(parentType, type, value);
+	});
+	
+	
+	
+	
+	//UNTESTED, need to add some actual items to click on first
+	function addToCustomItem(parentType, type, value) {
+		for (let i = 0; i < availableItems.length; i++) {
+			if (parentType == availableItems[i].name) {
+				var properties = Object.keys(availableItems[i]);
+				
+				for (let j = 0; j < properties.length; j++) {
+					
+					if (type == properties[i]) {
+						properties[i] == value;
+					}
+				}
+			}
+		}
+	}
 	
 	function refreshCart() {
 		//on page load/reload, sets the items of the local cart variable to those saved in the local storage
