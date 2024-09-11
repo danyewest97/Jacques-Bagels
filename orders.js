6092f3344b7f525839f23ee7f0a077c17c2a9f0e
danyewest97 <dane20052008@gmail.com>
2024-09-11 13:04:25 -0700

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,32 +1,54 @@
 $(document).ready(function() {
 	
+	//An array of all available toppings (more will be/need to be added)
+	const toppings = [
+		"lemon pepper",
+		"salt",
+		"pepper"
+	];
+	
+	//An array of all available spreads (more will be/need to be added)
+	const spreads = [
+		"butter",
+		"cream cheese",
+		"lox"
+	];
+	
+	//An array of all available bagel types (more will be/need to be added)
+	const types = [
+		"plain",
+		"sesame",
+		"everything"
+	];
+	
+	
 	const cart = [];
 	
-	//on page load/reload, sets the items of the local cart variable to those saved in the local storage
-	//This makes it so the user's cart items stay, even if they reload the page or go to another page
-	try {
-		const temp = JSON.parse(localStorage.getItem("cart"));
-		for (let i = 0; i < temp.length; i++) {
-			cart[i] = temp[i];
-		}
-	} catch(err) {
-		
-	}
+	refreshCart();
 	
 	//This is a customBagel object that stores an array of toppings
 	const customBagel = {
+		//common things on a bagel that can be changed by the user
 		toppings: [],
 		toasted: true,
-		spread: "";
+		spread: "",
+		type: "",
 		
+		//notes is used for if the user has a specific request for their bagel
+		notes: ""
 	};
 	
 	
 	$("p").click(function() {
-		cart.push(item.name);
+		cart.push(customBagel);
 		localStorage.setItem("cart", JSON.stringify(cart));
 		console.log(JSON.parse(localStorage.getItem("cart")));
 	});
+	
+	
+	
+	
+	
 	
 	
 	//stores a boolean variable (isMySessionActive) that becomes true when the user loads the page
@@ -45,4 +67,18 @@ $(document).ready(function() {
 		localStorage.isMySessionActive = "true";
 	};
 	
+	
+	
+	function refreshCart() {
+		//on page load/reload, sets the items of the local cart variable to those saved in the local storage
+		//This makes it so the user's cart items stay, even if they reload the page or go to another page
+		try {
+			const temp = JSON.parse(localStorage.getItem("cart"));
+			for (let i = 0; i < temp.length; i++) {
+				cart[i] = temp[i];
+			}
+		} catch(err) {
+			
+		}
+	}
 });
